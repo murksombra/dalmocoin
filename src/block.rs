6e833fd4d2d870870dd24cf39809b8bcdabed88b
Dalmo Veras <verasnt@protonmail.com>
2022-05-03 21:27:14 -0300

    adds mine fn

--- src/block.rs
+++ src/block.rs
@@ -8,6 +8,7 @@ pub struct Block {
     pub previous_block_hash: BlockHash,
     pub nonce: u64,
     pub payload: String,
+    pub difficulty: u128,
 }
 
 impl Debug for Block {
@@ -31,6 +32,7 @@ impl Block {
         previous_block_hash: BlockHash,
         nonce: u64,
         payload: String,
+        difficulty: u128,
     ) -> Self {
         Block {
             index,
@@ -39,6 +41,17 @@ impl Block {
             previous_block_hash,
             nonce,
             payload,
+            difficulty,
+        }
+    }
+    pub fn mine(&mut self) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+            if check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
         }
     }
 }
@@ -51,6 +64,11 @@ impl Hashable for Block {
         bytes.extend(&self.previous_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
         bytes.extend(self.payload.as_bytes());
+        bytes.extend(&u128_bytes(&self.difficulty));
         bytes
     }
+}
+
+pub fn check_difficulty(hash: &BlockHash, difficulty: u128) -> bool {
+    difficulty > difficulty_bytes_as_u128(&hash)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use dalmocoinlib::*;
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis Block".to_owned());
-    let hash = block.hash();
-    block.hash = hash;
+    let mut block = Block::new(
+        0,
+        0,
+        vec![0; 32],
+        68496,
+        "Genesis Block".to_owned(),
+        0x0000ffffffffffffffffffffffffffff,
+    );
+    block.hash = block.hash();
     println!("{:?}", &block);
+    // block.mine();
+    // println!("{:?}", &block);
 }
