1f74dcee599e98e1bc7238c2f0d86131076fd72b
Dalmo Veras <verasnt@protonmail.com>
2022-05-11 21:20:18 -0300

    rename blockhash and add impl hashable for output

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,6 +1,18 @@
+use super::*;
+
 pub struct Output {
     pub destination: Address,
     pub value: u64,
+}
+
+impl Hashable for Output {
+    fn bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+        bytes.extend(self.destination.as_bytes());
+        bytes.extend(&u64_bytes(&self.value));
+
+        bytes
+    }
 }
 
 pub struct Transaction {
